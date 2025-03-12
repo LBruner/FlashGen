4fef4757e74bbf46962ba57aafe88ee2fce5f362
Leandro Bruner <leandrobruner15@gmail.com>
2025-03-12 10:01:50 -0300

    conserta problema de compatibilidade do tailwind com o postCSS

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type {Config} from "tailwindcss";
+
+const config: Config = {
+
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      typography: {
+        // Customize the prose variant here
+        'full': {
+          css: {
+            maxWidth: '100%', // Remove max-width
+          },
+        },
+      },
+      colors: {
+        customGray: '#EAEAEA',
+        customDarkBrown: '#E1AD7C',
+        customDarkBg: '#040D12',
+        customDarkFooter: '#18181B',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
+export default config;
